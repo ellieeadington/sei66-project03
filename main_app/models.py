d95e650d0c03b0a24372977a6b67a055c4c5cd26
ellieeadington <ellieeadington5@gmail.com>
2022-10-03 09:51:17 +0100

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+STARS = (
+    ('5', '★★★★★'),
+    ('4', '★★★★'),
+    ('3', '★★★'),
+    ('2', '★★'),
+    ('1', '★'),    
+)
+
+VARIETIES = (
+    ('A', 'Arabica'),
+    ('R', 'Robusta'),
+    ('L', 'Liberica'),
+    ('E', 'Excelsa')
+)
 # Create your models here.
 class Cafe:
     cafe_name = models.CharField(max_length=250),
@@ -12,7 +26,48 @@ class Cafe:
     address_country = models.CharField(250),
     address_postcode = models.CharField(10),
     cafe_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded"),
-    menu_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
+    menu_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded"),
+    cafe_website = models.CharField(max_length=1000)
+    
+    
+class Event:
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE),    
+    event_name = models.CharField(max_length=250),
+    event_description = models.CharField(max_length=1000),
+    event_weekday = models.CharField(max_length=20),
+    event_time_from = models.TimeField(),
+    event_time_to = models.TimeField(),
+    event_image = models.CharField(max_length=1000)
+    
+class Review:
+    datetime = models.DateTimeField(),
+    stars = models.CharField(max_length=2, choices=STARS, default=STARS[0][0]),
+    review_title = models.CharField(max_length=250),
+    review_body = models.CharField(max_length=1000)
+
+class CoffeeBean:
+    coffee_bean_name = models.CharField(max_length=250),
+    coffee_bean_variety =  models.CharField(max_length=1000, choices=VARIETIES, default=VARIETIES[0][0]),
+    coffee_bean_description = models.CharField(max_length=2000),
+    roastery_name = models.CharField(max_length=250),
+    date_harvested = models.CharField(max_length=250),
+    coffee_bean_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded"),
+    coffee_bean_location = models.CharField(max_length=250)
+    
+class CafeOpening:
+    weekday_open = models.CharField(max_length=250),
+    open_from = models.TimeField(),
+    open_from = models.TimeField(),
+    
+class BrewingMethod:
+    brewing_method = models.CharField(max_length=250),
+        
+    
+    
+    
+        
+
+    
     
     
     
