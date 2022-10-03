55d83f99c36c11de669f4dc11f8597b842d1ce89
sesemann <robsesemann@gmail.com>
2022-10-03 13:54:47 +0100

    index and details pages created with paths for cafe

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cafe
 
 # Register your models here.
+
+admin.site.register(Cafe)
--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cafes/', views.cafes_index, name='index'),
-    
+
     # ROB SECTION
+    path('cafes/<int:cafe_id>', views.cafes_detail, name='detail'),
+    path('cafes/create/', views.CafeCreate.as_view(), name='cafes_create'),
     
     
     
@@ -24,8 +26,8 @@ urlpatterns = [
     
     
     
-    # ROB SECTION
-    
+    # Ellie SECTION
+    # path()
     
     
     
