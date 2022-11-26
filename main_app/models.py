3c4a9e610a6e1c43163d06cc9e772c3d92529635
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-11-26 19:51:17 +0000

    formatted code

--- main_app/models.py
+++ main_app/models.py
@@ -7,7 +7,7 @@ STARS = (
     ('4', '★★★★'),
     ('3', '★★★'),
     ('2', '★★'),
-    ('1', '★'),    
+    ('1', '★'),
 )
 
 VARIETIES = (
@@ -15,7 +15,7 @@ VARIETIES = (
     ('R', 'Robusta'),
     ('L', 'Liberica'),
     ('E', 'Excelsa')
-)  
+)
 
 BREWINGMETHOD = (
     ('DB', 'Drip Brewed'),
@@ -29,18 +29,6 @@ BREWINGMETHOD = (
     ('AP', 'Aeropress')
 )
 
-class Profile(models.Model):
-     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-     is_cafe_owner = models.BooleanField(default=False, null = True)
-     
-     def __str__(self):
-        return f"is {self.user.username} a cafe owner? {self.user.profile.is_cafe_owner}"
-
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created: 
-#         Profile.objects.create(user=instance)
-
 TYPE = (
     ('KID', 'Children'),
     ('MUM', 'Parent and Children'),
@@ -51,62 +39,68 @@ TYPE = (
     ('ART', 'Arts and Crafts')
 )
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    is_cafe_owner = models.BooleanField(default=False, null=True)
+
+    def __str__(self):
+        return f"is {self.user.username} a cafe owner? {self.user.profile.is_cafe_owner}"
 class CoffeeBean(models.Model):
     name = models.CharField(max_length=250)
-    variety =  models.CharField(max_length=1000, choices=VARIETIES, default=VARIETIES[0][0])
+    variety = models.CharField(max_length=1000, choices=VARIETIES, default=VARIETIES[0][0])
     description = models.CharField(max_length=2000)
     roastery = models.CharField(max_length=250)
     date_harvested = models.CharField(max_length=250)
-    image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
+    image = models.ImageField(upload_to='main_app/static/uploads', default="no image uploaded")
     location = models.CharField(max_length=250)
-    
+
     def get_absolute_url(self):
-        return reverse('coffee_bean_edit', kwargs = {'cafe_id': self.id})
+        return reverse('coffee_bean_edit', kwargs={'cafe_id': self.id})
+
     def __str__(self):
         return self.variety
+
     def __str__(self):
         return self.roastery
 
 class Cafe(models.Model):
-    cafe_name = models.CharField(max_length=250, blank=True)
-    cafe_bio = models.CharField(max_length=2500, blank=True)
-    date_founded = models.DateField()
-    address_line_1 = models.CharField(max_length=250, blank=True)
-    address_line_2 = models.CharField(max_length=250, blank=True)
-    address_city = models.CharField(max_length=250, blank=True)
-    address_county = models.CharField(max_length=250, blank=True)
-    address_country = models.CharField(max_length=250, blank=True)
-    address_postcode = models.CharField(max_length=10, blank=True)
-    cafe_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
-    menu_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
+    cafe_name = models.CharField(max_length=250)
+    cafe_bio = models.CharField(max_length=2500)
+    date_founded = models.DateField(null=True)
+    address_line_1 = models.CharField(max_length=250)
+    address_line_2 = models.CharField(max_length=250, null=True)
+    address_city = models.CharField(max_length=250)
+    address_county = models.CharField(max_length=250)
+    address_country = models.CharField(max_length=250)
+    address_postcode = models.CharField(max_length=10)
+    cafe_image = models.ImageField(upload_to='main_app/static/uploads', default="no image uploaded")
+    menu_image = models.ImageField(upload_to='main_app/static/uploads', default="no image uploaded")
     cafe_website = models.CharField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    coffee_beans = models.ManyToManyField(CoffeeBean, blank=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    coffee_beans = models.ManyToManyField(CoffeeBean, null=True)
+
     def get_absolute_url(self):
-        return reverse('detail', kwargs = {'cafe_id': self.id})
-    
+        return reverse('detail', kwargs={'cafe_id': self.id})
+
     def __str__(self):
         return self.address_city
-
 class Event(models.Model):
-
-    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)   
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     event_name = models.CharField(max_length=250)
     event_description = models.CharField(max_length=1000)
     event_type = models.CharField(max_length=4, choices=TYPE, default=TYPE[0][0])
     event_date = models.DateField()
     event_time_from = models.TimeField()
     event_time_to = models.TimeField()
-    event_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
+    event_image = models.ImageField(upload_to='main_app/static/uploads', default="no image uploaded")
 
     def get_absolute_url(self):
-        return reverse('event_edit', kwargs = {'cafe_id': self.id})
+        return reverse('event_edit', kwargs={'cafe_id': self.id})
+
     def __str__(self):
         return f"{self.get_event_type_display}"
 
-        
-    
+
 class Review(models.Model):
     datetime = models.DateField(auto_now_add=True, null=True)
     stars = models.CharField(max_length=2, choices=STARS, default=STARS[0][0])
@@ -114,21 +108,19 @@ class Review(models.Model):
     review_body = models.CharField(max_length=1000)
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    
-class CafeOpening(models.Model):
-    weekday_open = models.CharField(max_length=250)
-    open_from = models.TimeField()
-    open_from = models.TimeField()
-    
+
+# class CafeOpening(models.Model):
+#     weekday_open = models.CharField(max_length=250)
+#     open_from = models.TimeField()
+#     open_from = models.TimeField()
 class BrewingMethod(models.Model):
     method_name = models.CharField(max_length=2, choices=BREWINGMETHOD, default=BREWINGMETHOD[0][0])
-    method_image = models.ImageField(upload_to ='main_app/static/uploads', default="no image uploaded")
+    method_image = models.ImageField(upload_to='main_app/static/uploads', default="no image uploaded")
     method_bio = models.CharField(max_length=300)
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
 
-
     def get_absolute_url(self):
-        return reverse('brewing_method_edit', kwargs = {'cafe_id': self.id})
+        return reverse('brewing_method_edit', kwargs={'cafe_id': self.id})
 
     def __str__(self):
         return f"{self.get_method_name_display} on {self.method_bio}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,76 +1,44 @@
 from django import views
 from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
+    # home
     path('', views.home, name='home'),
+    # about
     path('about/', views.about, name='about'),
+    # cafes
     path('cafes/', views.cafes_index, name='index'),
-
-    # ROB SECTION
     path('cafes/<int:cafe_id>/', views.cafes_detail, name='detail'),
-    
-    # Delete and Update for Cafes
     path('cafes/<int:pk>/update/', views.CafeUpdate.as_view(), name='cafes_update'),
     path('cafes/<int:pk>/delete/', views.CafeDelete.as_view(), name='cafes_delete'),
-    
+    path('cafes/<int:cafe_id>/add_review/', views.add_review, name='add_review'),
     path('cafes/<int:cafe_id>/add_brewing_method/', views.add_brewing_method, name='add_brewing_method'),
-    path('profile/cafe/<int:cafe_id>/brewing_methods/update', views.brewing_method_edit, name='brewing_method_edit'),
-
     path('cafes/<int:cafe_id>/add_event/', views.add_event, name='add_event'),
-    path('profile/cafe/<int:cafe_id>/events/', views.event_edit, name='event_edit'),
-
-    # ASHISH SECTIO
-    # path('profile/update/', views.has_profile_submitted, name='has_profile_submitted'),
-    
-    path('accounts/password/reset',  
-     auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset.html'
-             ),
-        name='password_reset'
-    ),
-
-    path('accounts/password/reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ),
-        name= 'password_reset_done'
-    ),
-    path('accounts/password/reset/confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ),
-        name = 'password_reset_confirm'
-    ),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ),
-    name='password_reset_complete'),
-
-    # ELLIE SECTION
+    path('cafes/<int:pk>/event_update/', views.EventUpdate.as_view(), name='event_update'),
+    # coffee beans
     path('coffee_beans/', views.coffee_beans_index, name='coffee_beans_index'),
     path('coffee_beans/<int:coffee_beans_id>/', views.coffee_beans_detail, name='coffee_beans_detail'),
-
+    # auth
     path('signup/', views.signup, name="signup"),
-    path('signup/register-cafe/', views.register_cafe, name="register_cafe"),
-    path('signup/register-cafe/new-cafe', views.cafe_form, name="cafe_form"),
-    path('profile/', views.profile, name='profile'),
+    path('signup/register_cafe/', views.register_cafe, name="register_cafe"),
+    path('signup/add_cafe', views.cafe_create, name="cafe_create"),
+    path('accounts/password/reset',   auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),name='password_reset'),
+    path('accounts/password/reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),name= 'password_reset_done'),
+    path('accounts/password/reset/confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name = 'password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
+    # user
     path('user/update/<int:pk>/', views.UserUpdate.as_view(), name='user_update'),
-    
+    # profile
+    path('profile/', views.profile, name='profile'),
+    path('profile/cafe/<int:cafe_id>/brewing_methods/update', views.brewing_method_edit, name='brewing_method_edit'),
+    path('profile/cafe/<int:cafe_id>/events/', views.event_edit, name='event_edit'),
     path('profile/cafe/<int:cafe_id>/coffee_beans/', views.coffee_bean_edit, name='coffee_bean_edit'),
     path('profile/cafe/<int:cafe_id>/coffee_beans/add_coffee_bean/', views.add_coffee_bean, name='add_coffee_bean'),
     path('profile/cafe/<int:pk>/coffee_beans/update/', views.CoffeeBeanUpdate.as_view(), name='coffee_bean_update'),
     path('profile/cafe/<int:pk>/coffee_beans/delete/', views.CoffeeBeanDelete.as_view(), name='coffee_bean_delete'),
-    
-    path('cafes/<int:cafe_id>/add_review/', views.add_review, name='add_review'),
-    
-    path('search/', views.search, name="search"),
-
     path('profile/cafe/<int:pk>/brewing_methods/delete/', views.BrewingMethodDelete.as_view(), name='brewing_method_delete'),
-    
-    
+    # search
+    path('search/', views.search, name="search"),
 ]
